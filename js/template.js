be0aa1cd29ada04ae9530cf9639f7c9b07905df9
小林秀太 <k.i.shuta0603@gmail.com>
2021-07-19 19:43:41 +0900

    improve the design

---
+++ js/template.js
@@ -0,0 +1,32 @@
+function header() {
+    var html = "";
+    html += '<div class="container">';
+    html += '<header class="blog-header py-3">';
+    html += '<div class="row flex-nowrap justify-content-between align-items-center">';
+    html += '<div class="col-4 pt-1">'
+    html += '</div>'
+    html += '<div class="col-4 text-center">'
+    html += '<a class="blog-header-logo text-dark" href="https://kobashuu.github.io/Physics/physics-main.html">物理演習まとめ</a>'
+    html += '</div>'
+    html += '<div class="col-4 d-flex justify-content-end align-items-center">'
+    html += '</div>'
+    html += '</div>'
+    html += '</header>'
+    html += '</div>'
+    document.write(html);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
